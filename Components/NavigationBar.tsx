bf4a913189601d7058cbc7de0eb26210f3460fae
Jeet Paul <jeetpaul2222@gmail.com>
2026-01-20 21:03:01 +0530

    improve nav styling

--- Components/NavigationBar.tsx
+++ Components/NavigationBar.tsx
@@ -14,20 +14,28 @@ const NavigationBar = () => {
   ];
   return (
     <nav className="sticky top-0 z-50 w-full border-b bg-white/70 backdrop-blur-md">
-      <div className="flex justify-between items-center px-35 py-3">
-        <Link href={"/"} className="flex items-center gap-3">
+      <div className="flex justify-between items-center md:px-35 md:py-3 px-8 py-3">
+        <Link href={"/"} className="flex items-center md:gap-3 gap-1">
           <Image
             src="/LightLogo.png"
             alt="Logo"
-            width={40}
-            height={40}
-            className="bg-black rounded-lg"
+            width={0}
+            height={0}
+            className="bg-black rounded-lg size-7 md:size-10"
           />
-          <h1 className="font-bold text-2xl">Saelix UI</h1>
+          <h1 className="font-bold text-xl md:text-2xl">Saelix UI</h1>
         </Link>
         <div className="md:flex md:gap-8 md:items-center hidden">
           {NavMenu.map((menu) => (
-            <Link key={menu.name} href={menu.path}>
+            <Link
+              key={menu.name}
+              href={menu.path}
+              className="relative text-sm font-medium text-gray-600 transition-colors
+                         hover:text-gray-900
+                         after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-0
+                         after:bg-gray-900 after:transition-all after:duration-300
+                         hover:after:w-full"
+            >
               {menu.name}
             </Link>
           ))}
@@ -35,28 +43,52 @@ const NavigationBar = () => {
             <Moon className="size-5 text-gray-600" />
           </button>
         </div>
-        <div className="md:hidden">
+
+        <div className="md:hidden flex justify-center gap-3">
+          <div className="md:hidden">
+            <button className="bg-gray-700 hover:bg-gray-800 cursor-pointer px-2 shadow-xl py-2 rounded-lg">
+              <Moon className="size-5 text-gray-200" />
+            </button>
+          </div>
           <button
             onClick={() => setOpen(!open)}
-            className="flex flex-col justify-center gap-1.5 rounded-md
-                       border border-gray-300 p-2 transition hover:bg-gray-100"
+            className="flex flex-col justify-center gap-1.5 rounded-md p-2 transition bg-gray-700 hover:bg-gray-800 cursor-pointer"
           >
             <span
-              className={`h-0.5 w-5 bg-gray-900 transition-all duration-300 ${
+              className={`h-0.5 w-5 bg-gray-300 transition-all duration-300 ${
                 open ? "translate-y-2 rotate-45" : ""
               }`}
             />
             <span
-              className={`h-0.5 w-5 bg-gray-900 transition-all duration-300 ${
+              className={`h-0.5 w-5 bg-gray-300 transition-all duration-300 ${
                 open ? "opacity-0" : ""
               }`}
             />
             <span
-              className={`h-0.5 w-5 bg-gray-900 transition-all duration-300 ${
+              className={`h-0.5 w-5 bg-gray-300 transition-all duration-300 ${
                 open ? "-translate-y-2 -rotate-45" : ""
               }`}
             />
           </button>
+        </div>
+      </div>
+      <div
+        className={`overflow-hidden transition-all duration-300 md:hidden ${
+          open ? "max-h-72" : "max-h-0"
+        }`}
+      >
+        <div className="mx-4 mb-4 rounded-lg bg-white">
+          {NavMenu.map((item) => (
+            <Link
+              key={item.name}
+              href={item.path}
+              onClick={() => setOpen(false)}
+              className="block px-4 py-3 text-sm font-medium text-gray-700
+                         transition hover:bg-gray-100 text-shadow-lg"
+            >
+              {item.name}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
