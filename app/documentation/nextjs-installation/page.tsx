06fcad08fa9f4e7830aeba33b384520ef65b8bcc
Jeet Paul <jeetpaul2222@gmail.com>
2026-01-24 18:03:11 +0530

    feat: add NextJs and Vite installation components for documentation

---
+++ app/documentation/nextjs-installation/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NextJs_Installation_page = () => {
+  return (
+    <div>NextJs_Installation_</div>
+  )
+}
+
+export default NextJs_Installation_page
---
+++ app/documentation/vite-installation/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const vite_Installetion_page = () => {
+  return (
+    <div>Vite_page</div>
+  )
+}
+
+export default vite_Installetion_page
