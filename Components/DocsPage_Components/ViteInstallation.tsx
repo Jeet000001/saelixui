a0668d78e7ddb6a7bc58aa3228a315845b1653c3
Jeet Paul <jeetpaul2222@gmail.com>
2026-01-27 20:23:32 +0530

    feat: enhance ViteInstallation component with Tailwind CSS setup instructions

--- Components/DocsPage_Components/ViteInstallation.tsx
+++ Components/DocsPage_Components/ViteInstallation.tsx
@@ -7,9 +7,13 @@ import { useState } from "react";
 
 const ViteInstallation = () => {
   const [viteCommandCopy, setViteCommandCopy] = useState(false);
+  const [tailwindcssCommandCopy, setTailwindcssCommandCopy] = useState(false);
+  const [importCommandCopy, setImportCommandCopy] = useState(false);
   const [addCommandCopy, setAddCommandCopy] = useState(false);
   const [codeCopy, setCodeCopy] = useState(false);
   const viteCommand = "npm create vite@latest";
+  const tailwindcssCommand = "npm install tailwindcss @tailwindcss/vite";
+  const importCommand = `@import "tailwindcss";`;
   const addCommand = "npx saelix-ui add button";
   const code = `import { Button } from "@/components/ui/button"
  
@@ -45,15 +49,9 @@ export default function Home() {
             Vite.
           </span>{" "}
           During the setup process, select the{" "}
-          <span className="text-gray-900">
-            React
-          </span>{" "}
-           +{" "}
-          <span className="text-gray-900">
-            TypeScript
-          </span>{" "}
-          TypeScript template to ensure type safety and a modern development
-          setup.
+          <span className="text-gray-900">React</span> +{" "}
+          <span className="text-gray-900">TypeScript</span> TypeScript template
+          to ensure type safety and a modern development setup.
         </p>
 
         {/* Code Block */}
@@ -71,6 +69,78 @@ export default function Home() {
             </button>
           </div>
           <p className="text-gray-700 text-shadow-lg">npm create vite@latest</p>
+        </div>
+      </section>
+      <section className="space-y-4">
+        <h2 className="group text-shadow-lg relative inline-block text-2xl font-semibold text-transparent bg-clip-text bg-linear-to-b from-black to-gray-500">
+          Add Tailwind CSS
+          <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-gray-700 transition-all duration-300 group-hover:w-full" />
+        </h2>
+
+        {/* Code Block */}
+        <div className="relative rounded-xl border border-gray-200 hover:scale-105 transition-all duration-300 order-gray-200 bg-gray-50 px-4 py-3 font-mono text-sm text-gray-800 shadow-sm">
+          <div className="absolute top-2 right-2">
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(tailwindcssCommand);
+                setTailwindcssCommandCopy(true);
+                setTimeout(() => setTailwindcssCommandCopy(false), 2000);
+              }}
+              className="flex items-center gap-1 rounded-md active:scale-90 px-2 py-1 text-xs text-gray-800 hover:bg-gray-100 transition"
+            >
+              {tailwindcssCommandCopy ? (
+                <Check size={14} />
+              ) : (
+                <Copy size={14} />
+              )}
+            </button>
+          </div>
+          <p className="text-gray-700 text-shadow-lg">
+            npm install tailwindcss @tailwindcss/vite
+          </p>
+        </div>
+
+        <p className="text-gray-500">
+          Open the{" "}
+          <span className="bg-gray-50 px-2 py-0.5 rounded-lg border border-gray-200 shadow-sm">
+            src/index.css
+          </span>{" "}
+          file and remove any existing styles. Then, paste the content provided
+          below to replace it entirely.
+        </p>
+
+        <div className="relative overflow-hidden bg-gray-50 border-gray-200 hover:scale-105 transition-all duration-300 rounded-xl border shadow-sm">
+          {/* Header */}
+          <div className="flex items-center justify-between border-b border-gray-200 px-4 py-2">
+            <div className="flex items-center gap-2 text-xs">
+              <span className="rounded-sm px-2 py-0.5 bg-gray-900 text-white">
+                CSS
+              </span>
+              <span className="text-slate-400">src/index.css</span>
+            </div>
+
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(importCommand);
+                setImportCommandCopy(true);
+                setTimeout(() => setImportCommandCopy(false), 2000);
+              }}
+              className="rounded-md p-1 text-gray-800 transition"
+            >
+              {importCommandCopy ? <Check size={14} /> : <Copy size={14} />}
+            </button>
+          </div>
+
+          {/* Code */}
+          <pre className="rounded-xl p-4 text-sm">
+            <SyntaxHighlighter
+              language="jsx"
+              style={prism}
+              customStyle={{ background: "transparent" }}
+            >
+              {importCommand}
+            </SyntaxHighlighter>
+          </pre>
         </div>
       </section>
       <section className="space-y-4">
