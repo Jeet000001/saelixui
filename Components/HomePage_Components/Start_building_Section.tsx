40c607fb5a57b218200efceee40fea7dc42ef8cf
Jeet Paul <jeetpaul2222@gmail.com>
2026-01-29 21:24:15 +0530

    add start building section

---
+++ Components/HomePage_Components/Start_building_Section.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Start_building_Section = () => {
+  return (
+    <div>
+      <p>Start building</p>
+      <h1>
+        Build modern interfaces <span>faster</span>
+      </h1>
+      <p>
+        Saelix UI is a modern React component library built for speed,
+        accessibility, and consistency. Create beautiful interfaces with less
+        effort and more control.
+      </p>
+      <button>Explore Saelix UI</button>
+    </div>
+  );
+};
+
+export default Start_building_Section;
