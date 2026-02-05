7f0b9eaf74d89e65a99c990549143061eb2fac50
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-05 23:09:14 +0530

    add disabled button component

--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -3,7 +3,7 @@ import ShowComponent from "@/Components/ShowComponent";
 const ComponentPage = () => {
   return (
     <div>
-       <div className="ml-26 mb-5">
+      <div className="ml-26 mb-5">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
           Button
         </h1>
@@ -17,12 +17,12 @@ const ComponentPage = () => {
         <ShowComponent
           ComponentName="01. Primary"
           installation="npx saelix-ui add primary-button"
-          ComponentCodeTS={`import { Primary_Button } from "@/components/ui/Primary-button";
+          ComponentCodeTS={`import Primary_Button from "@/components/ui/Primary-button";
 
 const PrimaryButtonDemo = () => <Button>Primary</Button>;
 
 export default PrimaryButtonDemo;`}
-          ComponentCodeJS={`import { Primary_Button } from "./components/ui/Primary-button";
+          ComponentCodeJS={`import Primary_Button from "./components/ui/Primary-button";
 
 const PrimaryButtonDemo = () => <Button>Primary</Button>;
 
@@ -38,52 +38,133 @@ export default PrimaryButtonDemo;`}
         </ShowComponent>
         {/* Secondary Button */}
         <ShowComponent
-        ComponentName="02. Secondary"
+          ComponentName="02. Secondary"
           installation="npx saelix-ui add secondary-button"
-          ComponentCodeTS={`import { Secondary_Button } from "@/components/ui/Secondary-button";
+          ComponentCodeTS={`import Secondary_Button from "@/components/ui/Secondary-button";
 
 const SecondaryButtonDemo = () => <Button>Secondary</Button>;
 
 export default SecondaryButtonDemo;`}
-          ComponentCodeJS={`import { Secondary_Button } from "./components/ui/Secondary-button";
+          ComponentCodeJS={`import Secondary_Button from "./components/ui/Secondary-button";
 
 const SecondaryButtonDemo = () => <Button>Secondary</Button>;
 
-export default SecondaryButtonDemo;`}>
-          <button className="bg-gray-100 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 text-black dark:text-white border-b-2 border-gray-200 dark:border-b-2 dark:border-gray-600 px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">Secondary</button>
+export default SecondaryButtonDemo;`}
+        >
+          <button className="bg-gray-100 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 text-black dark:text-white border-b-2 border-gray-200 dark:border-b-2 dark:border-gray-600 px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">
+            Secondary
+          </button>
         </ShowComponent>
         {/* Destructive Button */}
         <ShowComponent
-        ComponentName="03. Destructive"
+          ComponentName="03. Destructive"
           installation="npx saelix-ui add destructive-button"
-          ComponentCodeTS={`import { Destructive_Button } from "@/components/ui/Destructive-button";
+          ComponentCodeTS={`import Destructive_Button from "@/components/ui/Destructive-button";
 
 const DestructiveButtonDemo = () => <Button>Destructive</Button>;
 
 export default DestructiveButtonDemo;`}
-          ComponentCodeJS={`import { Destructive_Button } from "./components/ui/Destructive-button";
+          ComponentCodeJS={`import Destructive_Button from "./components/ui/Destructive-button";
 
 const DestructiveButtonDemo = () => <Button>Destructive</Button>;
 
-export default DestructiveButtonDemo;`}>
-          <button className="bg-red-600 hover:bg-red-500 dark:bg-red-500 dark:hover:bg-red-400 text-white border-b-2 border-red-400 dark:border-b-2 dark:border-red-400 px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer ">Destructive</button>
+export default DestructiveButtonDemo;`}
+        >
+          <button className="bg-red-600 hover:bg-red-500 dark:bg-red-500 dark:hover:bg-red-400 text-white border-b-2 border-red-400 dark:border-b-2 dark:border-red-400 px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer ">
+            Destructive
+          </button>
         </ShowComponent>
         {/* Outline Button */}
         <ShowComponent
-        ComponentName="04. Outline"
+          ComponentName="04. Outline"
           installation="npx saelix-ui add outline-button"
-          ComponentCodeTS={`import { Outline_Button } from "@/components/ui/Outline-button";
+          ComponentCodeTS={`import Outline_Button from "@/components/ui/Outline-button";
 
 const OutlineButtonDemo = () => <Button>Outline</Button>;
 
 export default OutlineButtonDemo;`}
-          ComponentCodeJS={`import { Outline_Button } from "./components/ui/Outline-button";
+          ComponentCodeJS={`import Outline_Button from "./components/ui/Outline-button";
 
 const OutlineButtonDemo = () => <Button>Outline</Button>;
 
-export default OutlineButtonDemo;`}>
-          <button className="border border-gray-300 hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-950 dark:text-white px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">Outline</button>
+export default OutlineButtonDemo;`}
+        >
+          <button className="border border-gray-300 hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-950 dark:text-white px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">
+            Outline
+          </button>
         </ShowComponent>
+        {/* Ghost Button */}
+        <ShowComponent
+          ComponentName="05. Ghost"
+          installation="npx saelix-ui add ghost-button"
+          ComponentCodeTS={`import Ghost_Button from "@/components/ui/Ghost-button";
+
+const GhostButtonDemo = () => <Button>Ghost</Button>;
+
+export default GhostButtonDemo;`}
+          ComponentCodeJS={`import Ghost_Button from "./components/ui/Ghost-button";
+
+const GhostButtonDemo = () => <Button>Ghost</Button>;
+
+export default GhostButtonDemo;`}
+        >
+          <button className="hover:bg-gray-100 dark:hover:bg-gray-800 dark:text-white px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">
+            Ghost
+          </button>
+        </ShowComponent>
+        {/* Link Button */}
+        <ShowComponent
+          ComponentName="06. Link"
+          installation="npx saelix-ui add link-button"
+          ComponentCodeTS={`import Link_Button from "@/components/ui/Link-button";
+
+const LinkButtonDemo = () => <Button>Link</Button>;
+
+export default LinkButtonDemo;`}
+          ComponentCodeJS={`import Ghost_Button from "./components/ui/Link-button";
+
+const LinkButtonDemo = () => <Button>Link</Button>;
+
+export default LinkButtonDemo;`}
+        >
+          <button className="hover:underline dark:text-white dark:hover:underline px-5 py-2 rounded-xl active:scale-95 transition duration-300 ease-in-out cursor-pointer">
+            Link
+          </button>
+        </ShowComponent>
+        {/* Disabled Button */}
+        <ShowComponent
+          ComponentName="07. Disabled"
+          installation="npx saelix-ui add disabled-button"
+          ComponentCodeTS={`import Disabled_Button from "@/components/ui/Disabled-button";
+
+const DisabledButtonDemo = () => <Button>Disabled</Button>;
+
+export default DisabledButtonDemo;`}
+          ComponentCodeJS={`import Disabled_Button from "./components/ui/Disabled-button";
+
+const DisabledButtonDemo = () => <Button>Disabled</Button>;
+
+export default DisabledButtonDemo;`}
+        >
+          <button
+            disabled
+            className="
+        px-5 py-2 rounded-xl border-b-2 transition duration-300 ease-in-out
+        bg-black text-white border-gray-600
+        hover:bg-gray-900 active:scale-95
+        dark:bg-white dark:text-black dark:border-gray-400
+        dark:hover:bg-gray-100
+        disabled:bg-gray-400
+        disabled:text-gray-700
+        disabled:border-gray-300
+        disabled:cursor-not-allowed
+        disabled:opacity-60
+        disabled:hover:bg-gray-400 disabled:active:scale-100"
+          >
+            Disabled
+          </button>
+        </ShowComponent>
+        {/*  */}
       </div>
     </div>
   );
