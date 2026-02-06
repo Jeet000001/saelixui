c27166fd5432e91d0d2700a15f3d630576087705
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-06 22:55:06 +0530

    app accordion page

---
+++ app/components/accordion/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const accordion_page = () => {
+  return (
+    <div>accordian_page</div>
+  )
+}
+
+export default accordion_page
--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -291,7 +291,63 @@ export default GradientAnimationButtonDemo;`}
             </style>
           </>
         </ShowComponent>
-         {/* Text Glowing Button */}
+        {/* Text Glowing Button */}
+        <ShowComponent
+          ComponentName="12. Animated Text"
+          installation="npx saelix-ui add animated-text-button"
+          ComponentCodeTS={`import Animated_Text_Button from "@/components/ui/Animated-text-button";
+
+const AnimatiedtextButtonDemo = () => {
+  return <button>Hover Me</button>;
+};
+export default AnimatiedtextButtonDemo;;
+`}
+          ComponentCodeJS={`import Animated_Text_Button from "./components/ui/Animated-text-button";
+
+const AnimatiedtextButtonDemo = () => {
+  return <button>Hover Me</button>;
+};
+export default AnimatiedtextButtonDemo;`}
+        >
+          <>
+            <style>
+              {`
+        @keyframes glow {
+          0% { background-position: 200% 0; }
+          100% { background-position: -200% 0; }
+        }
+
+        .glow-text {
+          transition: all 0.3s ease-in-out;
+        }
+
+        /* Light mode hover */
+        .glow-hover:hover .glow-text {
+          color: transparent;
+          background-image: linear-gradient(to right, #ffffff, #9ca3af);
+          background-size: 200% auto;
+          background-clip: text;
+          -webkit-background-clip: text;
+          animation: glow 2s linear infinite;
+        }
+
+        /* Dark mode hover */
+        .dark .glow-hover:hover .glow-text {
+          background-image: linear-gradient(to right, #000000, #4b5563);
+        }
+        `}
+            </style>
+
+            <button
+              className="px-5 py-2 rounded-xl relative overflow-hidden glow-hover
+        bg-black text-white hover:bg-gray-950
+        dark:bg-white dark:text-black dark:hover:bg-gray-200
+        transition-all duration-300 active:scale-95"
+            >
+              <span className="relative inline-block glow-text">Hover Me</span>
+            </button>
+          </>
+        </ShowComponent>
       </div>
     </div>
   );
