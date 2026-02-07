9d479cece6b7ad2bc0ca9361b445badb9a738b48
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-07 23:07:13 +0530

    Add box accordion component add

---
+++ Components/Accordion/BoxAccordion.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+
+type AccordionItem = {
+  title: string;
+  content: string;
+};
+
+type AccordionProps = {
+  items: AccordionItem[];
+  allowMultiple?: boolean;
+};
+
+export const BoxAccordion = ({ items, allowMultiple = false }: AccordionProps) => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [openMulti, setOpenMulti] = useState<number[]>([]);
+
+  const toggleItem = (index: number) => {
+    if (allowMultiple) {
+      setOpenMulti((prev) =>
+        prev.includes(index)
+          ? prev.filter((i) => i !== index)
+          : [...prev, index]
+      );
+    } else {
+      setOpenIndex(openIndex === index ? null : index);
+    }
+  };
+
+  const isOpen = (index: number) => {
+    return allowMultiple ? openMulti.includes(index) : openIndex === index;
+  };
+
+  return (
+    <div className="w-full max-w-xl mx-auto border border-gray-300 dark:border-gray-800 rounded-xl overflow-hidden">
+      {items.map((item, index) => {
+        const open = isOpen(index);
+
+        return (
+          <div key={index} className="border-b border-gray-300 dark:border-gray-800 last:border-none">
+            {/* Header */}
+            <button
+              onClick={() => toggleItem(index)}
+              className="w-full flex justify-between items-center px-5 py-4 text-left font-medium hover:bg-gray-50 dark:hover:bg-gray-700 transition"
+              aria-expanded={open}
+            >
+              {item.title}
+
+              <ChevronDown
+                className={`transition-transform duration-300 ${
+                  open ? "rotate-180" : ""
+                }`}
+                size={18}
+              />
+            </button>
+
+            {/* Content */}
+            <div
+              className={`grid transition-all duration-300 ease-in-out ${
+                open ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
+              }`}
+            >
+              <div className="overflow-hidden">
+                <div className="px-5 pb-4 text-sm text-gray-600 dark:text-gray-300">
+                  {item.content}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- app/components/accordion/page.tsx
+++ app/components/accordion/page.tsx
@@ -1,3 +1,4 @@
+import { BoxAccordion } from "@/Components/Accordion/BoxAccordion";
 import { DefaultAccordion } from "@/Components/Accordion/DefaultAccordion";
 import ShowComponent from "@/Components/ShowComponent";
 
@@ -6,7 +7,7 @@ const accordion_page = () => {
     <div>
       <div className="ml-26 mb-5">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Button
+          Accordion
         </h1>
 
         <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
@@ -87,6 +88,80 @@ export default defaultAccordion;
               },
             ]}
           />
+        </ShowComponent>
+        {/*  */}
+        <ShowComponent
+          ComponentName="02. Box"
+          installation="npx saelix-ui add box-accordion"
+          ComponentCodeTS={`import { Box_Accordion } from "@/components/ui/Box-accordion";
+
+const boxAccordion = () => {
+  return (
+    <Default_Accordion
+      items={[
+        {
+          title: "What is this component?",
+          content: "A reusable accordion to show and hide content sections.",
+        },
+        {
+          title: "Can I customize it?",
+          content: "Yes, fully customizable with Tailwind and your styles.",
+        },
+        {
+          title: "Responsive?",
+          content: "Works smoothly on mobile, tablet, and desktop.",
+        },
+      ]}
+    />
+  );
+};
+
+export default boxAccordion;
+`}
+          ComponentCodeJS={`import { Box_Accordion } from "./components/ui/Box-accordion";
+
+const boxAccordion = () => {
+  return (
+    <Box_Accordion
+      items={[
+        {
+          title: "What is this component?",
+          content: "A reusable accordion to show and hide content sections.",
+        },
+        {
+          title: "Can I customize it?",
+          content: "Yes, fully customizable with Tailwind and your styles.",
+        },
+        {
+          title: "Responsive?",
+          content: "Works smoothly on mobile, tablet, and desktop.",
+        },
+      ]}
+    />
+  );
+};
+
+export default boxAccordion;
+`}
+        >
+          <BoxAccordion
+            items={[
+              {
+                title: "What is this component?",
+                content:
+                  "A reusable accordion to show and hide content sections.",
+              },
+              {
+                title: "Can I customize it?",
+                content:
+                  "Yes, fully customizable with Tailwind and your styles.",
+              },
+              {
+                title: "Responsive?",
+                content: "Works smoothly on mobile, tablet, and desktop.",
+              },
+            ]}
+          /> 
         </ShowComponent>
       </div>
     </div>
