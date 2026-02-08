cbc576e9fde153d2b375d7893037c07196fee1e6
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-08 11:06:47 +0530

    create avter page

---
+++ app/components/avatar/page.tsx
@@ -0,0 +1,20 @@
+const page = () => {
+  return (
+    <div>
+        <div className="ml-26 mb-5">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Avatar
+        </h1>
+
+        <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
+          Displays a user profile image with a fallback placeholder when the image is unavailable.
+        </p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
