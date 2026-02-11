9bed0f7b54b97d9fa9ba602e69683a13a4d288ef
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-11 19:22:51 +0530

    Fix button component

--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -181,7 +181,7 @@ const RoundedButtonDemo = () => <Button>Rounded</Button>;
 export default RoundedButtonDemo;`}
         >
           <button className="bg-black hover:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 text-white dark:text-black border-b-2 border-gray-600 dark:border-b-2 dark:border-gray-400 px-5 py-2 rounded-full active:scale-95 transition duration-300 ease-in-out cursor-pointer">
-            Disabled
+            Rounded
           </button>
         </ShowComponent>
         {/* Icon Button */}
