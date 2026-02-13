1e9a50e2f702b61716040ed432609f52a8d7d1fd
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-14 00:10:00 +0530

    add default breadcrumb

--- app/components/breadcrumb/page.tsx
+++ app/components/breadcrumb/page.tsx
@@ -12,12 +12,64 @@ const page = () => {
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
           Breadcrumb
         </h1>
-
         <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
           Renders a button or any element styled to behave like a button.
         </p>
       </div>
+      <div>
+                {/* Default */}
+        <ShowComponent
+          ComponentName="01. Default"
+          installation="npx saelix-ui add default-breadcrumb"
+          ComponentCodeTS={`import Breadcrumb from "@/components/ui/Default-breadcrumb";
 
+const DefaultBreadcrumbDemo = () => {
+  return (
+    <div>
+      <Breadcrumb
+        separator="/"
+        items={[
+          { label: "Home", href: "/routeLink" },
+          { label: "Docs", href: "/routeLink" },
+          { label: "Components", href: "/routeLink" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default DefaultBreadcrumbDemo;
+`}
+          ComponentCodeJS={`import Breadcrumb from "./components/ui/Default-breadcrumb";
+
+const DefaultBreadcrumbDemo = () => {
+  return (
+    <div>
+      <Breadcrumb
+        separator="/"
+        items={[
+          { label: "Home", href: "/routeLink" },
+          { label: "Docs", href: "/routeLink" },
+          { label: "Components", href: "/routeLink" },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default DefaultBreadcrumbDemo;
+`}
+        >
+          <Default_Breadcrumb
+            separator="/"
+            items={[
+              { label: "Home", href: " " },
+              { label: "Docs", href: " " },
+              { label: "Components", href: " " },
+            ]}
+          />
+        </ShowComponent>
+      </div>
     </div>
   );
 };
@@ -29,58 +81,7 @@ export default page;
 
 
 //       <div>
-//         {/* Default */}
-//         <ShowComponent
-//           ComponentName="01. Default"
-//           installation="npx saelix-ui add default-breadcrumb"
-//           ComponentCodeTS={`import Breadcrumb from "@/components/ui/Default-breadcrumb";
 
-// const DefaultBreadcrumbDemo = () => {
-//   return (
-//     <div>
-//       <Breadcrumb
-//         separator="/"
-//         items={[
-//           { label: "Home", href: "/routeLink" },
-//           { label: "Docs", href: "/routeLink" },
-//           { label: "Components", href: "/routeLink" },
-//         ]}
-//       />
-//     </div>
-//   );
-// };
-
-// export default DefaultBreadcrumbDemo;
-// `}
-//           ComponentCodeJS={`import Breadcrumb from "./components/ui/Default-breadcrumb";
-
-// const DefaultBreadcrumbDemo = () => {
-//   return (
-//     <div>
-//       <Breadcrumb
-//         separator="/"
-//         items={[
-//           { label: "Home", href: "/routeLink" },
-//           { label: "Docs", href: "/routeLink" },
-//           { label: "Components", href: "/routeLink" },
-//         ]}
-//       />
-//     </div>
-//   );
-// };
-
-// export default DefaultBreadcrumbDemo;
-// `}
-//         >
-//           <Default_Breadcrumb
-//             separator="/"
-//             items={[
-//               { label: "Home", href: " " },
-//               { label: "Docs", href: " " },
-//               { label: "Components", href: " " },
-//             ]}
-//           />
-//         </ShowComponent>
 //         {/* Icon */}
 //         <ShowComponent
 //           ComponentName="02. Icon"
