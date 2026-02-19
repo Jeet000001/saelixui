a89b9b60ff25fde30cc6250b03032216dedd408a
Jeet Paul <jeetpaul2222@gmail.com>
2026-02-19 19:33:10 +0530

    Add carousel component

---
+++ app/components/carousel/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="ml-26 mb-5">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Carousel
+        </h1>
+        <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
+          Displays content in a compact and organized format.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default page;
