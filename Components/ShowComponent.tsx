20cf027e320cf045f2ccd667d14012c2829ecb4a
Jeet Paul <jeetpaul2222@gmail.com>
2026-03-02 23:56:36 +0530

    formate the showcomponent component

--- Components/ShowComponent.tsx
+++ Components/ShowComponent.tsx
@@ -3,7 +3,10 @@
 import { useState } from "react";
 import { Code, X, Copy, Check } from "lucide-react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneLight, oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import {
+  oneLight,
+  oneDark,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useTheme } from "next-themes";
 
 type ShowComponentProps = {
@@ -32,11 +35,14 @@ export default function ShowComponent({
 
   return (
     <div className="flex justify-center items-start px-4 sm:px-6 md:px-10 py-4">
-      <div className="w-full max-w-3xl rounded-xl border border-gray-200 bg-white shadow-sm
-        dark:bg-[#111827] dark:border-[#1F2937] dark:shadow-[0_10px_30px_rgba(0,0,0,0.6)]">
-
-        <div className="flex items-center justify-between border-b border-gray-300 px-4 py-3
-          dark:border-[#1F2937]">
+      <div
+        className="w-full max-w-3xl rounded-xl border border-gray-200 bg-white shadow-sm
+        dark:bg-[#111827] dark:border-[#1F2937] dark:shadow-[0_10px_30px_rgba(0,0,0,0.6)]"
+      >
+        <div
+          className="flex items-center justify-between border-b border-gray-300 px-4 py-3
+          dark:border-[#1F2937]"
+        >
           <h2 className="font-semibold text-lg dark:text-[#E5E7EB]">
             {ComponentName}
           </h2>
@@ -56,13 +62,14 @@ export default function ShowComponent({
 
       {open && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
-          
-          <div className="w-full max-w-2xl max-h-130 rounded-xl bg-white flex flex-col
-            dark:bg-[#111827] dark:shadow-[0_10px_30px_rgba(0,0,0,0.6)]">
-
-            <div className="sticky top-0 z-20 border-b rounded-t-xl border-gray-300 pb-2 bg-white
-              dark:bg-[#111827] dark:border-[#1F2937]">
-
+          <div
+            className="w-full max-w-2xl max-h-130 rounded-xl bg-white flex flex-col
+            dark:bg-[#111827] dark:shadow-[0_10px_30px_rgba(0,0,0,0.6)]"
+          >
+            <div
+              className="sticky top-0 z-20 border-b rounded-t-xl border-gray-300 pb-2 bg-white
+              dark:bg-[#111827] dark:border-[#1F2937]"
+            >
               <div className="flex items-center justify-between px-4 py-3">
                 <h3 className="font-semibold dark:text-[#E5E7EB]">
                   Installation
@@ -102,8 +109,10 @@ export default function ShowComponent({
                 </div>
               </div>
 
-              <div className="mx-5 mb-3 flex items-center justify-between gap-2 rounded-lg border-2 border-gray-200 px-4 py-3 text-sm
-                dark:bg-[#0B0F19] dark:border-[#1F2937] dark:text-[#E5E7EB]">
+              <div
+                className="mx-5 mb-3 flex items-center justify-between gap-2 rounded-lg border-2 border-gray-200 px-4 py-3 text-sm
+                dark:bg-[#0B0F19] dark:border-[#1F2937] dark:text-[#E5E7EB]"
+              >
                 <code className="truncate">{installation}</code>
 
                 <button
@@ -115,13 +124,16 @@ export default function ShowComponent({
                   className="rounded-md px-2 py-1 text-xs hover:bg-gray-200
                     dark:hover:bg-[#1F2937] dark:text-[#3B82F6]"
                 >
-                  {installationCodeCopied ? <Check size={14} /> : <Copy size={14} />}
+                  {installationCodeCopied ? (
+                    <Check size={14} />
+                  ) : (
+                    <Copy size={14} />
+                  )}
                 </button>
               </div>
             </div>
 
             <div className="relative flex-1 overflow-y-auto no-scrollbar">
-              
               <div className="sticky top-4 z-10 flex justify-end pr-6 pt-4">
                 <button
                   onClick={async () => {
@@ -157,9 +169,7 @@ export default function ShowComponent({
                   </SyntaxHighlighter>
                 </pre>
               </div>
-
             </div>
-
           </div>
         </div>
       )}
