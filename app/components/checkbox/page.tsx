68b53e749361c629dbe36de16f0394bb7c0faeb6
Jeet Paul <jeetpaul2222@gmail.com>
2026-03-02 23:15:26 +0530

    set the stracture of checkbox page

--- app/components/checkbox/page.tsx
+++ app/components/checkbox/page.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        
+        <div className="ml-26 mb-5">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Checkbox
+        </h1>
+        <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
+          A small toggle component used to represent a yes-or-no choice.
+        </p>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
