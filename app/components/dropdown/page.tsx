05fa83776d46147d203b40398d88563ddc82809f
Jeet Paul <jeetpaul2222@gmail.com>
2026-03-05 00:50:57 +0530

    Add stracture od dropdown page

--- app/components/dropdown/page.tsx
+++ app/components/dropdown/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <div className="ml-26 mb-5">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Dropdown
+        </h1>
+        <p className="mt-3 text-base leading-relaxed text-gray-600 dark:text-gray-400 max-w-2xl">
+          Shows a dropdown menu containing various actions or options when the
+          user clicks a button.
+        </p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
